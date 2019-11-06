21b9674c18b6a9b3c51fcf2b2f97c0eae4c212e3
oare <49346313+oare@users.noreply.github.com>
2019-11-06 09:49:34 +0100

    add personal touch to brew.sh

--- brew.sh
+++ brew.sh
@@ -53,49 +53,76 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# brew install aircrack-ng
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install netpbm
+# brew install nmap
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
-brew install ack
+# brew install ack
 #brew install exiv2
 brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
+# brew install git-lfs
+# brew install gs
+# brew install imagemagick --with-webp
+# brew install lua
+# brew install lynx
 brew install p7zip
 brew install pigz
-brew install pv
+# brew install pv
 brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
+#brew install rlwrap
+# brew install ssh-copy-id
+# brew install tree
+# brew install vbindiff
+# brew install zopfli
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+# My brews
+
+brew install cocoapods
+brew install docker
+brew install docker-machine
+brew install git-extras
+brew install gradle
+brew install icu4c
+brew install p7zip
+brew install pwgen
+brew install thefuck
+brew install wget
+brew install zsh
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+
+# My casks
+
+brew cask install chrome
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install station
+brew cask install mtmr
+brew cask install provisionql
+brew cask install spotify
+brew cask install virtualbox
